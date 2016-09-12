b85be8072a5679048968d1998088491ddb5420fa
Dan904 <daniel_linge@yahoo.com>
2016-09-11 23:36:55 -0400

    moved note one line up

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,7 +10,9 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim tmux.conf bash_aliases profile"    # list of files/folders to symlink in homedir
+
+# list of files/folders to symlink in homedir
+files="bashrc vimrc vim tmux.conf bash_aliases profile"  
 
 ##########
 
